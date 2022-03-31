09a5e3cfa5e6da911498cf4748cf994c402b1491
maninaik <maninaik111@gmail.com>
2022-04-01 01:17:44 +0530

    Added a lightweight jwt for middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,22 +1,22 @@
 import { NextResponse } from 'next/server';
-import { verifyToken } from '../lib/utils';
+const jwt = require('@tsndr/cloudflare-worker-jwt'); //light weight jwt bcz middleware is an edge func
 
-export function middleware(req, ev) {
+export async function middleware(req) {
 	const token = req.cookies?.token || null;
-	const userId = verifyToken(token);
+	let userId;
+	if (token) {
+		userId = await jwt.verify(token, process.env.JWT_SECRET);
+	}
 	const { pathname } = req.nextUrl;
-	console.log({ pathname });
 	if (
 		userId ||
 		pathname.includes('/api/login') ||
 		pathname.includes('/static')
 	) {
-		console.log('herer');
 		return NextResponse.next();
 	}
 
 	if (!token && pathname !== '/login') {
-		console.log('redirected');
 		const url = req.nextUrl.clone();
 		url.pathname = '/login';
 		return NextResponse.rewrite(url);
